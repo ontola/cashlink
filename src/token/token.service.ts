4de1986a7d4404792b7aff2e5cd62e83d110d072
Joep Meindertsma <joep@ontola.io>
2021-04-05 11:59:44 +0200

    WIP jose crypto

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -3,10 +3,15 @@ import { Injectable } from '@nestjs/common';
 
 import { ConfigService } from '@nestjs/config';
 import { Token } from './token.interface';
+import jose from 'node-jose';
 
 @Injectable()
 export class TokenService {
+  /** Authorized Uphold SDK instance for communicating with the API */
   sdk: any;
+  /** JOSE Key Store for storing cryptographic keys */
+  keystore: any;
+
   constructor(private configService: ConfigService) {
     /** Initializes communication with Uphold API */
     const sdk = new SDK({
@@ -16,6 +21,7 @@ export class TokenService {
       clientSecret: this.configService.get<string>('UPHOLD_CLIENT_SECRET'),
     });
     this.sdk = sdk;
+    this.keystore = jose.JWK.createKeyStore();
 
     sdk
       // What do I put here?
@@ -24,6 +30,20 @@ export class TokenService {
       .then(() => sdk.getMe())
       .then((user) => {
         console.log('sdk', user);
+      });
+  }
+
+  /** Encrypts a token, returns a base64 string */
+  encrypt(token: Token): Promise<string> {
+    const key = this.keystore.get();
+    return jose.JWE.createEncrypt(key)
+      .update(JSON.stringify(token))
+      .final()
+      .then(function (result) {
+        const output: string = jose.util.base64url.encode(result, 'utf8');
+        return output;
+        // {result} is a JSON Object -- JWE using the JSON General Serialization
+        // Might need this: https://github.com/cisco/node-jose#uri-safe-base64
       });
   }
 
