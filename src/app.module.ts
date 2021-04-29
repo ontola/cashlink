d5957368c25a9467c590760b0c7fd699148efca1
Joep Meindertsma <joep@ontola.io>
2021-04-29 11:27:29 +0200

    Remove user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TokenModule } from './token/token.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    UserModule,
     TypeOrmModule.forRoot(),
     TokenModule,
     ConfigModule.forRoot({
